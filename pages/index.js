e4e5e80ec5953b92311c2c54fb5e571b64a602f6
annkanokwon <annkanokwon@gmail.com>
2022-04-25 10:44:27 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>fbclone</title>
+        <meta name="description" content="fbclone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+
+      </main>
+
+    </div>
+  )
+}
